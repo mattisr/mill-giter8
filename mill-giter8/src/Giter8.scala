f7c8a3d7fc29cc2212cd45bf36e6779ef7ed76c7
mattias <p.mattias.rundgren@gmail.com>
2020-11-12 08:27:33 +0100

    Added scaladoc

--- mill-giter8/src/Giter8.scala
+++ mill-giter8/src/Giter8.scala
@@ -11,37 +11,33 @@ object Giter8 extends mill.define.ExternalModule {
 
   def hello(name: String) = T.command{println(s"Hello $name")}
 
-  def instantiate(repo: String, // The git repo path
-                  ref: String = "", // Branch or tag
-                  directoryInRepo: String = "", // Subdirectory within cloned repo
-                  outputFolder: String = "", // Output directory
-                  knownHosts: String = "", // ?
-                  properties: Map[String, String] = Map(),
-                  overwriteFiles: Boolean = false
+  /**
+   *
+   * @param repo See [[giter8.GitRepository.Matches]]
+   * @param ref Branch.
+   * @param directoryInRepo Only used if non standard, i.e. none of src/main/g8, root.
+   * @param outputFolder Defaults to the name property.
+   * @param knownHosts Git known hosts
+   * @param properties A property map. E.g. Map("name" -> "My Project")
+   * @param overwriteFiles Defaults to false.
+   * @return Either a success message, or a error message.
+   */
+  def instantiate(repo: String, ref: String = "", directoryInRepo: String = "", outputFolder: String = "",
+                  knownHosts: String = "", properties: Map[String, String] = Map(), overwriteFiles: Boolean = false
                  ) = T.command {
-    val Param = """(?s)^--(\S+)=(.+)$""".r
     val tempdir = new File(os.temp.dir().toIO, "giter8-" + System.nanoTime)
     val workingDir: File = new File(".")
     val arguments: Seq[String] = properties.map{ a => s"--${a._1}=${a._2}"}.toList
-    val outputDir = outputFolder match {case o:String if o.length > 0 => Some(o);case _ => None}
+    val toOption: (String) => Option[String] = s => s match {case o:String if o.length > 0 => Some(o);case _ => None}
     for {
-      conf <- createConfig(repo, None/*ref*/, Some(directoryInRepo), outputDir, None/*knownHosts*/)
+      conf <- Right(Config(repo, toOption(ref).map(Ref.Branch), overwriteFiles, Some(directoryInRepo),
+        toOption(outputFolder), toOption(knownHosts)))
       repo <- GitRepository.fromString(repo)
       cloneDir <- cloneRepo(repo, conf.ref, tempdir, conf.knownHosts)
       templateDir = new File(cloneDir, conf.directory.getOrElse(""))
-      result <- G8TemplateRenderer.render(templateDir, workingDir, arguments, overwriteFiles,
+      result <- G8TemplateRenderer.render(templateDir, workingDir, arguments, conf.forceOverwrite,
         conf.out.map(new File(_)))
     } yield result
-  }
-
-
-  def createConfig(repo: String,
-                   ref: Option[Ref] = None,
-                   directory: Option[String] = None,
-                   out: Option[String],
-                   knownHosts: Option[String] = None
-                  ): Either[String, Config] = {
-    Right(Config(repo, ref, forceOverwrite = true, directory, out, knownHosts))
   }
 
   def cloneRepo(repo: GitRepository, ref: Option[Ref], tempdir: File, knownHosts: Option[String]): Either[String, File] =
